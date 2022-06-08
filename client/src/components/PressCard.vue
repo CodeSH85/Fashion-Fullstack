0ccce5edf58becc2d5e29b1e7bea3c9a303a135e
ziggykay <kaylachen155@gmail.com>
2022-06-08 12:29:16 +0800

    修改scss檔名

--- client/src/components/PressCard.vue
+++ client/src/components/PressCard.vue
@@ -1,49 +1,47 @@
 <template>
-     <div :class="[{border: has_border}, 'press-card']">
-         <div class="image-box">
-           <img class="card-img-top" :src="img"  alt="picture">
-         </div>
-            <!-- <h5 class="card-title">{{ title }}</h5> -->
-           <slot></slot>
+  <div :class="[{ border: has_border }, 'press-card']">
+    <div class="image-box">
+      <img class="press-img" :src="img" alt="picture" />
     </div>
+    <!-- <h5 class="card-title">{{ title }}</h5> -->
+    <slot></slot>
+  </div>
 </template>
 <script>
 export default {
   props: {
-    'id': Number,
-    'title': String,
-    'img': String,
+    id: Number,
+    title: String,
+    img: String,
   },
   methods: {
-    parseImgPath: function(){
-      return this.$store.state.pressInfo.img
-    }
-  }
-}
+    parseImgPath: function () {
+      return this.$store.state.pressInfo.img;
+    },
+  },
+};
 </script>
 <style scoped>
-  .border{
-    border: 5px solid #f3c303;
-  }
-  div.press-card{
-    min-width: 350px;
-    width: 24%;
-    text-align: center;
-    margin: 1px;
-    cursor: pointer;
-    display: block;
-  }
-  div.press-card .image-box{
-    width: 100%;
-    height: 250px;
-    display: flex;
-  }
- img{
-    display: block;
-    width: 50%;
-    margin: auto;
-  }
-  small{
-    color: #bc0f0f
-  }
+div.press-card {
+  /* width: 24%; */
+  text-align: center;
+  /* margin: 1px; */
+  cursor: pointer;
+  display: block;
+}
+div.press-card .image-box {
+  width: 100%;
+  min-width: 275px;
+  /* height: 380px; */
+  display: flex;
+}
+img {
+  width: 263px;
+  height: 329px;
+  display: block;
+  margin: auto;
+}
+small {
+  color: #bc0f0f;
+}
 </style>
