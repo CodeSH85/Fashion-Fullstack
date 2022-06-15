5ffe462a51b1778c527f371c29c342c603a9b168
ziggykay <kaylachen155@gmail.com>
2022-06-15 16:08:41 +0800

    20220615

--- client/src/components/NavbarComponent.vue
+++ client/src/components/NavbarComponent.vue
@@ -28,16 +28,16 @@
           class="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span class="navbar-toggler-icon">三</span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
               <router-link to="/" class="nav-link active" aria-current="page"
                 >HOME</router-link
