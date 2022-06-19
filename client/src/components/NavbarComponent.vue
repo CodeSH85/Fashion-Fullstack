56970fcdcf8007783e333d1ddd65cef28367fe9e
ziggykay <kaylachen155@gmail.com>
2022-06-19 13:07:53 +0800

    fix bugs

--- client/src/components/NavbarComponent.vue
+++ client/src/components/NavbarComponent.vue
@@ -4,91 +4,95 @@
     class="topnav navbar-p-3 navbar-expand-lg px-5"
   >
     <!-- topnav -->
-    <router-link to="/" >
-    <div class="topnav-brand">    
-        <div class="logo-img"></div>      
-    </div>
+    <router-link to="/">
+      <div class="topnav-brand">
+        <div class="logo-img"></div>
+      </div>
     </router-link>
 
     <div class="item-list container-fluid p-0">
       <ul class="d-flex m-0">
         <li class="topnav-item">
           <router-link to="/login">Sign in</router-link>
-      <router-link to="/">
-        <div class="logo-img"></div>
-      </router-link>
+          <router-link to="/">
+            <div class="logo-img"></div>
+          </router-link>
+        </li>
+      </ul>
     </div>
     <div class="item-list container-fluid p-0">
       <ul class="d-flex m-0">
         <li class="topnav-item">
           <router-link to="/account" v-if="login">Account</router-link>
-          <router-link to="/login" v-else >Sign in</router-link>
+          <router-link to="/login" v-else>Sign in</router-link>
         </li>
         <li class="topnav-item">
           <router-link to="/bag">Bag</router-link>
         </li>
       </ul>
     </div>
-    </nav>
-    <!-- navbar -->
-    <nav  :class="[darkModeClass, greyModeClass]" class="navbar navbar-p-3 navbar-expand-lg">
-      <div class="container-fluid" style="border: 2px solid #CC0000;">
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon">三</span>
-        </button>
+  </nav>
+  <!-- navbar -->
+  <nav
+    :class="[darkModeClass, greyModeClass]"
+    class="navbar navbar-p-3 navbar-expand-lg"
+  >
+    <div class="container-fluid" style="border: 2px solid #cc0000">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon">三</span>
+      </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <router-link to="/" class="nav-link active" aria-current="page"
-                >HOME</router-link
-              >
-            </li>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link active" aria-current="page"
+              >HOME</router-link
+            >
+          </li>
 
-            <li class="nav-item">
-              <router-link to="/lookbook/0" class="nav-link link"
-                >LOOKBOOK</router-link
-              >
-            </li>
+          <li class="nav-item">
+            <router-link to="/lookbook/0" class="nav-link link"
+              >LOOKBOOK</router-link
+            >
+          </li>
 
-            <li class="nav-item">
-              <router-link to="/press" class="nav-link link">PRESS</router-link>
-            </li>
+          <li class="nav-item">
+            <router-link to="/press" class="nav-link link">PRESS</router-link>
+          </li>
 
-            <li class="nav-item">
-              <router-link to="/about" class="nav-link link">ABOUT</router-link>
-            </li>
+          <li class="nav-item">
+            <router-link to="/about" class="nav-link link">ABOUT</router-link>
+          </li>
 
-            <li class="nav-item">
-              <router-link to="/shop" class="nav-link link">SHOP</router-link>
-            </li>
-          </ul>
-        </div>
+          <li class="nav-item">
+            <router-link to="/shop" class="nav-link link">SHOP</router-link>
+          </li>
+        </ul>
       </div>
-    
+    </div>
   </nav>
 </template>
 <script>
 export default {
   name: "NavbarComponent",
-  inject:['reload'],
-  data () {
+  inject: ["reload"],
+  data() {
     return {
       unlogin: true,
-    }
+    };
   },
   methods: {
-    loginStatus () {
-      let status = this.$store.state.loginUser
-      if (status === '') {
+    loginStatus() {
+      let status = this.$store.state.loginUser;
+      if (status === "") {
         this.unlogin = true;
         // this.reload();
       } else {
@@ -96,7 +100,7 @@ export default {
         // this.reload();
         return;
       }
-    }
+    },
   },
   computed: {
     darkModeClass() {
@@ -115,5 +119,5 @@ export default {
     //   }
     // }
   },
-}
+};
 </script>
--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -3,8 +3,9 @@
   <div class="container-fluid">
     <div class="row">
       <div class="h1_title d-flex justify-content-center">
-        <h1 class="h3 col-md-8 fw-bolder col-sm-3 text-md-nowrap text-sm-wrap text-md-start 
-        text-sm-center">
+        <h1
+          class="h3 col-md-8 fw-bolder col-sm-3 text-md-nowrap text-sm-wrap text-md-start text-sm-center"
+        >
           會員登入 SIGN IN
         </h1>
       </div>
@@ -12,116 +13,150 @@
       <div>
         <div class="email_block col-md-8 col-sm-10 mx-auto mt-5">
           <label for="email" class="email_title h5 mb-2">E-mail</label>
-          <input type="email" v-model="email" class="email_input form-control lh-lg" id="account" placeholder="" required>
+          <input
+            type="email"
+            v-model="email"
+            class="email_input form-control lh-lg"
+            id="account"
+            placeholder=""
+            required
+          />
         </div>
         <div class="password_block col-md-8 col-sm-10 mx-auto mt-5">
           <label for="password" class="password_title h5 mb-2">Password</label>
-          <input type="password" v-model="password" class="password_input form-control lh-lg" id="password" placeholder="" required>
+          <input
+            type="password"
+            v-model="password"
+            class="password_input form-control lh-lg"
+            id="password"
+            placeholder=""
+            required
+          />
         </div>
         <!-- 表格按鈕 (large_group是最外面那層，middle_group是另外包申請帳號/忘記密碼的那層) -->
-        <div class="buttons_large_group col-8 mx-auto d-flex justify-content-md-between mt-5 
-        flex-md-row flex-sm-column-reverse flex-md-nowrap flex-sm-wrap">
-          <div class="buttons_middle_group col-md-4 d-flex col-sm-6 flex-md-nowrap flex-sm-wrap 
-          mx-md-0 mx-sm-auto mt-md-0 mt-sm-4">
-            <div class="col-md-5 col-sm-12 h5 fw-bold my-3 mb-0 text-md-start text-sm-center">
-              <router-link to="/account/signup" type="submit" class="new_account_button text-dark pb-1">
+        <div
+          class="buttons_large_group col-8 mx-auto d-flex justify-content-md-between mt-5 flex-md-row flex-sm-column-reverse flex-md-nowrap flex-sm-wrap"
+        >
+          <div
+            class="buttons_middle_group col-md-4 d-flex col-sm-6 flex-md-nowrap flex-sm-wrap mx-md-0 mx-sm-auto mt-md-0 mt-sm-4"
+          >
+            <div
+              class="col-md-5 col-sm-12 h5 fw-bold my-3 mb-0 text-md-start text-sm-center"
+            >
+              <router-link
+                to="/account/signup"
+                type="submit"
+                class="new_account_button text-dark pb-1"
+              >
                 申請帳號
               </router-link>
             </div>
-            <div class="col-md-5 col-sm-12 h5 fw-bold my-3 mb-0 text-md-start text-sm-center">
-              <router-link to="#" type="submit" class="forget_password_button text-dark pb-1">
+            <div
+              class="col-md-5 col-sm-12 h5 fw-bold my-3 mb-0 text-md-start text-sm-center"
+            >
+              <router-link
+                to="#"
+                type="submit"
+                class="forget_password_button text-dark pb-1"
+              >
                 忘記密碼
               </router-link>
             </div>
           </div>
-          <div to="/account" type="submit" class="d-block col-md-3 col-sm-7 mx-md-0 mx-sm-auto mt-md-0 mt-sm-3">
-            <button class="sign_in_button h4 fw-bold link-dark text-center py-3 mb-0"
-              @click.prevent="login()">SIGN IN</button>
+          <div
+            to="/account"
+            type="submit"
+            class="d-block col-md-3 col-sm-7 mx-md-0 mx-sm-auto mt-md-0 mt-sm-3"
+          >
+            <button
+              class="sign_in_button h4 fw-bold link-dark text-center py-3 mb-0"
+              @click.prevent="login()"
+            >
+              SIGN IN
+            </button>
           </div>
         </div>
       </div>
     </div>
   </div>
-
 </template>
 <script>
-  //import axios from "axios"
+import axios from "axios";
 
-  export default {
-    data () {
-      return {
-        email:'',
-        password:'',
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+
+  // method:{
+  //   async login(){
+  //     let result = await axios.post("http://localhost:3000/post/login")
+  //   }
+  // },
+  // mounted(){
+
+  methods: {
+    // async login(){
+    //   let result = await axios.post("http://localhost:3001/post/login")
+    // }
+
+    signIn() {
+      // 將使用者輸入的email記到localStorage並傳值到store、導向到accountView頁面
+      const loginEmail = document.querySelector("#account");
+      const loginPassword = document.querySelector("#password");
+      const storageKey = "userAccount"; // 將帳號組成localStorage的key
+
+      // 建立localStorage (建立的key，要建立的value--將loginEmail的值設為value)
+      localStorage.setItem(storageKey, loginEmail.value);
+      // 將userName (登入時輸入的email) 從localStorage裡面取出來
+      const userName = localStorage.getItem(storageKey);
+      // 定義登入條件的判斷，若符合就在localStorage記下user帳號(userName = MemoryAccount)
+      const confirmMemory = loginEmail.value !== "";
+
+      loginEmail.value = "";
+      loginPassword.value = "";
+
+      if (confirmMemory) {
+        // 這裡再儲存一次是為了重新整理或關掉頁面，可以維持登入狀態
+        alert("登入成功");
+        localStorage.setItem("memoryAccount", `${userName}`);
+        this.$router.push("/account");
+      } else {
+        alert("請輸入E-mail帳號");
+        return;
       }
+
+      const nowUser = localStorage.getItem("memoryAccount");
+      this.$store.commit("getUser", {
+        memoryAccount: nowUser,
+      });
     },
-
-    // method:{
-    //   async login(){
-    //     let result = await axios.post("http://localhost:3000/post/login")
-    //   }
-    // },
-    // mounted(){
-
-    //methods:{
-      // async login(){
-      //   let result = await axios.post("http://localhost:3001/post/login")
-      // }
-
-      signIn () {
-        // 將使用者輸入的email記到localStorage並傳值到store、導向到accountView頁面
-        const loginEmail = document.querySelector ('#account')
-        const loginPassword = document.querySelector ('#password')
-        const storageKey = 'userAccount' // 將帳號組成localStorage的key
-
-        // 建立localStorage (建立的key，要建立的value--將loginEmail的值設為value)
-        localStorage.setItem (storageKey, loginEmail.value) 
-        // 將userName (登入時輸入的email) 從localStorage裡面取出來
-        const userName = localStorage.getItem (storageKey) 
-        // 定義登入條件的判斷，若符合就在localStorage記下user帳號(userName = MemoryAccount)
-        const confirmMemory = (loginEmail.value !== '')
-
-        loginEmail.value = ''
-        loginPassword.value = ''
-
-        if (confirmMemory) {
-          // 這裡再儲存一次是為了重新整理或關掉頁面，可以維持登入狀態
-          alert ('登入成功')
-          localStorage.setItem ('memoryAccount', `${userName}`)
-          this.$router.push('/account'); 
-        } else {
-          alert ('請輸入E-mail帳號')
-          return;
-        }
-
-        const nowUser = localStorage.getItem('memoryAccount')
-        this.$store.commit ('getUser', {
-          memoryAccount: nowUser,
-        })
-      }
-    },
-    mounted() {
-      login() {
-        axios.post('http://localhost:3000/post/login',{
+  },
+  mounted: {
+    login() {
+      axios
+        .post("http://localhost:3000/post/login", {
           email: this.email,
-          password: this.password
+          password: this.password,
         })
-        .then((res)=>{
-          if( res.data.status == 1 ){
-            alert('登入成功')
+        .then((res) => {
+          if (res.data.status == 1) {
+            alert("登入成功");
             // 將後端傳回來的狀態儲存至 localStorage( isLogin : "status:1" )
-            localStorage.setItem("isLogin",JSON.stringify(res.data)) 
-            return this.$router.push('/shop')
-          } else if( res.data.status == 0 ) {
-            alert('帳號或密碼錯誤')
-            return this.$router.push('/login')
+            localStorage.setItem("isLogin", JSON.stringify(res.data));
+            return this.$router.push("/shop");
+          } else if (res.data.status == 0) {
+            alert("帳號或密碼錯誤");
+            return this.$router.push("/login");
           }
         })
         .catch(function (error) {
           console.log(error);
-        }) 
-      }
-
-    }
-  }
-
+        });
+    },
+  },
+};
 </script>
